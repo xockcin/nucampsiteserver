56a6225a224eb6f40e796fcfea1b698e1763d1a7
Nick Cox <xoc.kcin@gmail.com>
2021-05-23 10:07:26 -0400

    basic authentication

--- app.js
+++ app.js
@@ -38,10 +38,35 @@ app.use(logger('dev'))
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 app.use(cookieParser())
+
+function auth(req,res,next) {
+  console.log(req.headers)
+  const authHeader = req.headers.authorization
+  if (!authHeader) {
+    const err = new Error('You are not authenticated!')
+    res.setHeader('WWW-Authenticate', 'Basic')
+    err.status = 401
+    return next(err)
+  }
+
+  const auth = Buffer.from(authHeader.split(' ')[1], 'base64').toString().split(':')
+  const [user, pass] = auth
+  if (user === "admin" && pass === "password") {
+    return next()
+  } else {
+    const err = new Error('You are not authorized')
+    res.setHeader("WWW-Authenticate", "Basic");
+    err.status = 401;
+    return next(err);
+  }
+}
+
+app.use(auth)
+
 app.use(express.static(path.join(__dirname, 'public')))
 
 app.use('/', indexRouter);
-// app.use('/users', usersRouter);
+app.use('/users', usersRouter);
 app.use('/campsites', campsiteRouter)
 app.use("/promotions", promotionRouter)
 app.use("/partners", partnerRouter)
