fabf38bac443ef4133c009050a04726ccd9ca565
Nick Cox <xoc.kcin@gmail.com>
2021-05-23 09:12:23 -0400

    week 2 workshop

--- app.js
+++ app.js
@@ -6,21 +6,45 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+const campsiteRouter = require('./routes/campsiteRouter')
+const promotionRouter = require("./routes/promotionRouter")
+const partnerRouter = require("./routes/partnerRouter")
 
-var app = express();
+const mongoose = require('mongoose')
+const url = "mongodb://localhost:27017/nucampsite";
+const connect = mongoose.connect(
+  url,
+  {
+    useCreateIndex: true,
+    useFindAndModify: false,
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  }
+)
+
+connect.then(
+  () => console.log("Connected correctly to server"),
+  err => console.log(err)
+)
+
+
+var app = express()
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'jade')
 
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(logger('dev'))
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+app.use(cookieParser())
+app.use(express.static(path.join(__dirname, 'public')))
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+// app.use('/users', usersRouter);
+app.use('/campsites', campsiteRouter)
+app.use("/promotions", promotionRouter)
+app.use("/partners", partnerRouter)
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
