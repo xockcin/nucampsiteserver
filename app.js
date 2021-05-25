e3d280edb283f48f30aaf12b0c9b3be7fe97a210
Nick Cox <xoc.kcin@gmail.com>
2021-05-24 21:59:53 -0400

    authentication etc - might not work

--- app.js
+++ app.js
@@ -51,35 +51,21 @@ app.use(
   })
 );
 
-function auth(req,res,next) {
-  console.log(req.session)
+app.use("/", indexRouter);
+app.use("/users", usersRouter);
+
+function auth(req, res, next) {
+  console.log(req.session);
 
   if (!req.session.user) {
-    const authHeader = req.headers.authorization;
-    if (!authHeader) {
-      const err = new Error("You are not authenticated!");
-      res.setHeader("WWW-Authenticate", "Basic");
-      err.status = 401;
-      return next(err);
-    }
-    const auth = Buffer.from(authHeader.split(" ")[1], "base64")
-      .toString()
-      .split(":");
-    const [user, pass] = auth;
-    if (user === "admin" && pass === "password") {
-      req.session.user = 'admin'
+    const err = new Error("You are not authenticated!");
+    err.status = 401;
+    return next(err);
+  } else {
+    if (req.session.user === "authenticated") {
       return next();
     } else {
-      const err = new Error("You are not authorized");
-      res.setHeader("WWW-Authenticate", "Basic");
-      err.status = 401;
-      return next(err);
-    }
-  } else {
-    if (req.session.user === 'admin') {
-      return next()
-    } else {
-      const err = new Error("You are not authorized");
+      const err = new Error("You are not authenticated!");
       err.status = 401;
       return next(err);
     }
@@ -90,8 +76,7 @@ app.use(auth)
 
 app.use(express.static(path.join(__dirname, 'public')))
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+
 app.use('/campsites', campsiteRouter)
 app.use("/promotions", promotionRouter)
 app.use("/partners", partnerRouter)
